2590af7c3858e047b0a6195f0fa705d42c5b82c8
caiqian <zero530@126.com>
2024-08-30 16:44:25 +0800

    add video time

--- imooc/get_course_conent.py
+++ imooc/get_course_conent.py
@@ -20,13 +20,20 @@ if response.status_code == 200:
         # 在当前父级元素中提取子元素
         # title = parent.xpath("//*[contains(@class, 'left')]")  # 获取标题内容
         title = parent.xpath(
-            "./*[contains(@class, 'name-box')]//*[contains(@class, 'left') and normalize-space(text())]/text()")  # 获取标题内容
-        content = parent.xpath("./ul//*[contains(@class, 'media-name')]/text()")  # 获取段落内容
+            "./*[contains(@class, 'name-box')]//*[contains(@class, 'left') and normalize-space(text())]/text()")
+
+        content_list = parent.xpath("./ul//*[contains(@class, 'media-name')]/text()")
+
+        time_list = parent.xpath("./ul//*[contains(@class, 'media-time')]/text()")
+
+        # 组合name和code
+        combined_texts = ['{} [{}]'.format(c.strip().replace('\n', ''), t.strip().replace('\n', '')) for
+                          c, t in zip(content_list, time_list)]
 
         # 遍历打印标题和段落内容
         for t in title:
             print(t.strip().replace('\n', ''))
-        for paragraph in content:
+        for paragraph in combined_texts:
             print(f"{paragraph.strip().replace('\n', '')}")
 else:
     print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
